f4ff9e91eb6abcb4c189360641d9fe3cdf599712
shravan01-01 <shriramsadawarti@gmail.com>
2026-07-17 19:07:10 +0530

    Refactor Product component to use React fragments for cleaner markup

--- src/product.jsx
+++ src/product.jsx
@@ -1,10 +1,10 @@
 function Product(){
     return(
-        <div>
+        <>
             <h3>Product</h3>
             <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="product" width="50" height="50"/>
             <p>Price: $19.99</p>
-        </div>
+        </>
     )
 }
 
